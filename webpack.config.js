fff4a045476760bc6422f9e61c96876014271125
Ifycode <marychord@gmail.com>
2024-12-05 18:33:51 +0100

    Fix: globalObject comment fix in webpack config - incase of Nextjs Apps

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    // debugging help: https://github.com/gregberge/loadable-components/issues/276#issuecomment-823797962
-    globalObject: 'this',
+    // debugging help e.g. for next.js: https://github.com/gregberge/loadable-components/issues/276#issuecomment-823797962
+    globalObject: `typeof self !== 'undefined' ? self : this`,
     library: 'react-lib-webpack',
     libraryTarget: 'umd'
   },
